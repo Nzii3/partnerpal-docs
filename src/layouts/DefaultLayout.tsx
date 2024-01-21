7d8d63e645aa8de5e1817dc6aaafec463f632da5
Nzii3 <exeyst@gmail.com>
2024-01-21 11:52:29 -0800

    ..

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -19,8 +19,8 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, child
         <>
             <Head>
                 <title>{ title } - { siteConfig.tagline }</title>
-                <meta name="description" content={ description } />
-                <meta property="og:description" content={ description } />
+                <meta name="description" content="Introducing Sentry: Your Multipurpose bot! Advanced commands, custom messages, custom punishments, applications, and customizable whitelists for enhanced control. Elevate your community with ease." />
+                <meta property="og:description" content="Introducing Sentry: Your Multipurpose bot! Advanced commands, custom messages, custom punishments, applications, and customizable whitelists for enhanced control. Elevate your community with ease." />
                 <meta property="og:title" content={ title } />
                 <meta name="theme-color" content="#5865f2" />
             </Head>
