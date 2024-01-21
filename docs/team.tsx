9f4ad1691a82b915ed7b487cf1c4710c2f932a9c
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2024-01-20 21:59:04 -0800

    Update team.tsx

---
+++ docs/team.tsx
@@ -0,0 +1,29 @@
+---
+title: 👑 Team
+---
+
+# Team
+
+<div class="card-demo">
+  <div class="card">
+    <div class="card__header">
+      <div class="avatar">
+        <img
+          class="avatar__photo"
+          src="https://r.nziie.xyz/avatar" />
+        <div class="avatar__intro">
+          <div class="avatar__name">vNziie--</div>
+          <small class="avatar__subtitle">
+            Head Developer
+          </small>
+        </div>
+      </div>
+    </div>
+    <div class="card__footer">
+      <div class="button-group button-group--block">
+        <a class="button button--primary" href="#url">Primary</a>
+        <a class="button button--primary" href="#url">Primary</a>
+      </div>
+    </div>
+  </div>
+</div>
