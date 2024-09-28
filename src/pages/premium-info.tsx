64267ee2a8802b2b37533e1cdce983d3c1933ba1
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2024-09-28 17:25:53 +0000

    .

--- src/pages/premium-info.tsx
+++ src/pages/premium-info.tsx
@@ -11,7 +11,7 @@ function Community() {
   const context = useDocusaurusContext();
 
   return (
-    <Layout title="Premium" description="Check out Sentry+ and some options to better enhance your server">
+    <Layout title="Premium" description="Check out PartnerPal premium to better enhance your server growth!">
       <header className="hero">
         <div className="container text--center">
           <h1 className='gold-cycle'>PartnerPal Premium</h1>
@@ -19,7 +19,7 @@ function Community() {
           <p>
           </p>
           </div>
-          <img className={classnames(styles.headerImg)} src="img/partnerpal-beta-icon.jpg" />
+          <img className={classnames(styles.headerImg)} src="img/partnerpal-beta-icon.png" />
         </div>
       </header>
       <main>
@@ -36,8 +36,8 @@ function Community() {
                 </div>
                 <div className="card__body">
                   <ul style={{'listStylePosition': 'inside'}}>
-                  <li>Lower cooldowns (5 minutes instead of 10 minutes for <code>/partner</code></li>
-                  <li>Partner with 10 servers at once every 30 minutes with <code>/partner-mass</code></li>
+                  <li>Lower cooldowns (5 minutes instead of 10 minutes for <mention>/partner</mention>)</li>
+                  <li>Partner with 10 servers at once every 30 minutes with <mention>/partner-mass</mention></li>
                   <li>Customizable advertisement banner (stand out)</li>
                   <li>Customizable advertisement embed color (stand out)</li>
                   <li>7 custom button actions instead of 3</li>
@@ -48,73 +48,6 @@ function Community() {
                 </div>
                 <div className="card__footer">
                   <Link to="/premium" className="button button--outline button--primary button--block">Purchase</Link>
-                </div>
-              </div>
-            </div>
-            
-            <div className="col text--center padding-vert--md">
-              <div className="card">
-                <div className="card__header">
-                <FontAwesomeIcon icon="fa-solid fa-sack-dollar" style={{'fontSize': '3em'}} bounce />
-                </div>
-                <div className="card__header">
-                <h2>Donations</h2>
-                </div>
-                <div className="card__body">
-                  We appreciate any donations! You will recieve the <strong style={{'color': '#5865f2'}}>Donator</strong> role in the support server to show our thanks :)
-                </div>
-                <div className="card__footer">
-                  <Link to="https://r.nziie.xyz/donate" className="button button--outline button--primary button--block">Donate</Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <h2 className="text--center" id="custom-branding">Custom Branding</h2>
-          <p className="text--center">Custom Branding allows you to fully customize Sentry to your needs. We offer a <strong style={{'color': '#5865f2'}}>custom bot name</strong>, <strong style={{'color': '#5865f2'}}>custom bot avatar</strong>, <strong style={{'color': '#5865f2'}}>custom bot status</strong>, and <strong style={{'color': '#5865f2'}}>full premium features</strong>. With even more control, you are allowed to create the bot application on the <a href="https://discord.dev">Discord Developer Portal</a>.</p>
-
-          <div className="row margin-vert--lg">
-          <div className="col text--center padding-vert--md">
-              <div className="card">
-                <div className="card__header">
-                <FontAwesomeIcon icon="fa-solid fa-medal" style={{'fontSize': '3em', 'color': '#C0C0C0'}} beat />
-                </div>
-                <div className="card__header">
-                <h2>Silver (4.99€/month)</h2>
-                </div>
-                <div className="card__body">
-                  <ul style={{'listStylePosition': 'inside'}}>
-                  <li>Customizable bot name</li>
-                  <li>Customizable bot avatar</li>
-                  <li>Customizable bot status</li>
-                  <li>Customizable bot about me</li>
-                  <li><strong style={{'color': '#ffd866'}}>Maximum of 1 server for the bot</strong></li>
-                  </ul>
-                </div>
-                <div className="card__footer">
-                  <Link to="/silver" className="button button--outline button--primary button--block">Purchase</Link>
-                </div>
-              </div>
-            </div>
-            <div className="col text--center padding-vert--md">
-              <div className="card">
-                <div className="card__header">
-                <FontAwesomeIcon icon="fa-solid fa-medal" style={{'fontSize': '3em', 'color': '#b68a07'}} beat />
-                </div>
-                <div className="card__header">
-                <h2>Gold (7.99€/month)</h2>
-                </div>
-                <div className="card__body">
-                  <ul style={{'listStylePosition': 'inside'}}>
-                  <li>Customizable bot name</li>
-                  <li>Customizable bot avatar</li>
-                  <li>Customizable bot status</li>
-                  <li>Customizable bot about me</li>
-                  <li><strong style={{'color': '#2ecc71'}}>Better performance</strong></li>
-                  <li><strong style={{'color': '#ffd866'}}>Maximum of 5 servers for the bot</strong></li>
-                  </ul>
-                </div>
-                <div className="card__footer">
-                  <Link to="/gold" className="button button--outline button--primary button--block">Purchase</Link>
                 </div>
               </div>
             </div>
