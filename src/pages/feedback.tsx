d1ae8e3a39297c7b7f28fa5bb25486d31d42bbdf
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2024-09-28 12:19:00 -0700

    Create feedback.tsx

---
+++ src/pages/feedback.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RedirectComponent from '../components/RedirectComponent';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+const RedirectPage: React.FC = () => {
+  return (
+    <div>
+      {/* Include the RedirectComponent for automatic redirection */}
+      <RedirectComponent url="https://lx4w0bpthcf.typeform.com/to/eUEdeu5X"/>
+    </div>
+  );
+};
+
+export default RedirectPage;
