4e1aef71752f91a83e07060ca45a1afac0896a0a
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2024-11-03 18:44:09 -0800

    Update premium.tsx

--- src/pages/premium.tsx
+++ src/pages/premium.tsx
@@ -32,7 +32,55 @@ function Community() {
                 <FontAwesomeIcon icon="fa-solid fa-gem" style={{'fontSize': '3em'}} bounce />
                 </div>
                 <div className="card__header">
-                <h2>Basic ($3.99/month)</h2>
+                <h2>1 Month Subscription ($3.99/month)</h2>
+                </div>
+                <div className="card__body">
+                  <ul style={{'listStylePosition': 'inside'}}>
+                  <li>Lower cooldowns for commands</li>
+                  <li>Partner with 5 servers at once every 45 minutes <strong>automatically</strong> with <mention>/partner-mass</mention> (turn on in <mention>/settings</mention>)</li>
+                  <li>Increased character limit for advertisement, <strong>4,000</strong> instead of <strong>2,000</strong> (use <mention>/settings</mention>)</li>
+                  <li>Customizable advertisement banner (<strong>stand out more</strong>)</li>
+                  <li>Customizable advertisement embed color (<strong>stand out more</strong>)</li>
+                  <li>7 custom button actions instead of 3</li>
+                  <li>7 custom menu option actions instead of 3</li>
+                  <li>Other increased limits</li>
+                  <li><a href="/support">Premium support</a></li>
+                  </ul>
+                </div>
+                <div className="card__footer">
+                  <Link to="https://partnerpal.mysellix.io/product/partnerpal-basic-premium-670c2a769fd31" className="button button--outline button--primary button--block">Subscribe</Link>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card__header">
+                <FontAwesomeIcon icon="fa-solid fa-gem" style={{'fontSize': '3em'}} bounce />
+                </div>
+                <div className="card__header">
+                <h2>3 Month Subscription ($10.99/month)</h2>
+                </div>
+                <div className="card__body">
+                  <ul style={{'listStylePosition': 'inside'}}>
+                  <li>Lower cooldowns for commands</li>
+                  <li>Partner with 5 servers at once every 45 minutes <strong>automatically</strong> with <mention>/partner-mass</mention> (turn on in <mention>/settings</mention>)</li>
+                  <li>Increased character limit for advertisement, <strong>4,000</strong> instead of <strong>2,000</strong> (use <mention>/settings</mention>)</li>
+                  <li>Customizable advertisement banner (<strong>stand out more</strong>)</li>
+                  <li>Customizable advertisement embed color (<strong>stand out more</strong>)</li>
+                  <li>7 custom button actions instead of 3</li>
+                  <li>7 custom menu option actions instead of 3</li>
+                  <li>Other increased limits</li>
+                  <li><a href="/support">Premium support</a></li>
+                  </ul>
+                </div>
+                <div className="card__footer">
+                  <Link to="https://partnerpal.mysellix.io/product/partnerpal-basic-premium-670c2a769fd31" className="button button--outline button--primary button--block">Subscribe</Link>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card__header">
+                <FontAwesomeIcon icon="fa-solid fa-gem" style={{'fontSize': '3em'}} bounce />
+                </div>
+                <div className="card__header">
+                <h2>1 Year Subscription ($36.99/month)</h2>
                 </div>
                 <div className="card__body">
                   <ul style={{'listStylePosition': 'inside'}}>
@@ -58,5 +106,3 @@ function Community() {
     </Layout>
   );
 }
-
-export default Community;
