2d81297ca186eba22929f9699f1ef7e8d55cd7ce
Thomas McGannon <exeyst@gmail.com>
2024-11-09 16:55:21 -0800

    Update vote.tsx

--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -5,8 +5,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 const RedirectPage: React.FC = () => {
   return (
     <div>
-      {/* Include the RedirectComponent for automatic redirection */}
-      <RedirectComponent url="https://top.gg/bot/1275655805093281863/vote"/>
+      <p>:)</p>
     </div>
   );
 };
